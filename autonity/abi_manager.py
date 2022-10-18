02fb0776a18d2c3d94d25566fb3a60c95cf4e521
Duncan Tebbs <duncan.tebbs@gmail.com>
2022-10-18 21:44:52 +0100

    ABIManager class

---
+++ autonity/abi_manager.py
@@ -0,0 +1,32 @@
+# Copyright (C) 2015-2022 Clearmatics Technologies Ltd - All Rights Reserved.
+
+"""
+ABIManager class
+"""
+
+import autonity.abi
+
+import importlib.resources  # type: ignore
+import json
+from web3.contract import ABI
+from typing import Dict
+
+
+_CACHE: Dict[str, ABI] = {}
+
+
+class ABIManager:
+    """
+    Access to bundled contract ABIs
+    """
+
+    @staticmethod
+    def load_abi(contract_name: str) -> ABI:
+        """
+        Load (and cache) the ABI for a protocol contract.
+        """
+        if contract_name not in _CACHE:
+            text = importlib.resources.read_text(autonity.abi, contract_name + ".abi")
+            _CACHE[contract_name] = json.loads(text)
+
+        return _CACHE[contract_name]
