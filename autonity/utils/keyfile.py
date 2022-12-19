1e2ae69ac408565dae75790d4cea1d65e0016c97
Duncan Tebbs <duncan.tebbs@gmail.com>
2022-12-19 18:18:03 +0000

    add get_address_from_private_key util function

--- autonity/utils/keyfile.py
+++ autonity/utils/keyfile.py
@@ -4,6 +4,7 @@
 Keyfile utility functions
 """
 
+from eth_account import Account
 from eth_keyfile import create_keyfile_json, decode_keyfile_json  # type: ignore
 from web3 import Web3
 from web3.types import ChecksumAddress
@@ -24,6 +25,14 @@ def load_keyfile(filename: str) -> EncryptedKeyData:
         keyfile_json = json.load(keyfile_f)
         # TODO: validate the json
         return cast(EncryptedKeyData, keyfile_json)
+
+
+def get_address_from_private_key(private_key: PrivateKey) -> ChecksumAddress:
+    """
+    Return the address associated with a given private key.
+    """
+    account = Account.from_key(private_key)  # pylint: disable=no-value-for-parameter
+    return account.address
 
 
 def create_keyfile_from_private_key(
