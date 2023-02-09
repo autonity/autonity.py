d4cfe8069039bc1c4ac0f373ff7a31dd26a013f6
Duncan Tebbs <duncan.tebbs@gmail.com>
2023-02-09 11:08:13 +0000

    add support for parsing contract return values

--- autonity/abi_parser.py
+++ autonity/abi_parser.py
@@ -9,10 +9,57 @@ from __future__ import annotations
 from web3 import Web3
 from web3.types import (
     ABI,
+    ABIFunction,
     ABIFunctionParams,
-    Sequence,
 )
-from typing import List, Any, Union, Callable
+from typing import Dict, List, Tuple, Sequence, Any, Union, Callable, cast
+
+
+def find_abi_function(abi: ABI, function_name: str) -> ABIFunction:
+    """
+    Given an ABI and function name, find the ABIFunction element.
+    """
+
+    for element in abi:
+        if element["type"] == "function" and element["name"] == function_name:
+            return cast(ABIFunction, element)
+
+    raise ValueError(f"function {function_name} not found in ABI")
+
+
+def parse_arguments(abi_function: ABIFunction, arguments: List[str]) -> List[Any]:
+    """
+    Given an ABI, a function name and a list of string parameters,
+    parse the parameters to suitable types to call the function on a
+    contract with the given ABI.
+    """
+
+    inputs = abi_function["inputs"]
+    parsers = _argument_parsers_for_params(inputs)
+    if len(parsers) != len(arguments):
+        raise ValueError(f"function requires {len(parsers)}, received {len(arguments)}")
+
+    return [parse(arg) for parse, arg in zip(parsers, arguments)]
+
+
+def parse_return_value(abi_function: ABIFunction, return_value: Any) -> Any:
+    """
+    Top level entry point, given the ABI outputs.  Supports void
+    types, and flattening a one-element tuple to a raw type.
+    """
+
+    outputs = abi_function["outputs"]
+    if len(outputs) == 0:
+        return None
+
+    if len(outputs) == 1:
+        # Single return value (including an array)
+        return _parse_return_value_from_type(
+            outputs[0]["type"], outputs[0], return_value
+        )
+
+    assert isinstance(return_value, tuple)
+    return _parse_return_value_tuple(outputs, return_value)
 
 
 ParamType = Union[str, int, float, bool]
@@ -26,14 +73,14 @@ Function to parse a string to a ParamType
 """
 
 
-def parse_string(value: str) -> str:
+def _parse_string(value: str) -> str:
     """
     Identity parser.
     """
     return value
 
 
-def parse_bool(bool_str: str) -> bool:
+def _parse_bool(bool_str: str) -> bool:
     """
     Boolean parser.
     """
@@ -43,7 +90,7 @@ def parse_bool(bool_str: str) -> bool:
     return True
 
 
-def string_to_argument_fn_for_type(arg_type: str) -> ParamParser:
+def _string_to_argument_fn_for_type(arg_type: str) -> ParamParser:
     """
     Return a function which parses a string into a type suitable for
     function arguments.
@@ -57,18 +104,18 @@ def string_to_argument_fn_for_type(arg_type: str) -> ParamParser:
     if arg_type.startswith("uint") or arg_type.startswith("int"):
         return int
     if arg_type == "bool":
-        return parse_bool
+        return _parse_bool
     if arg_type == "address":
         return Web3.toChecksumAddress
     if arg_type.startswith("bytes") or arg_type == "string":
-        return parse_string
+        return _parse_string
     if arg_type.startswith("fixed") or arg_type.startswith("ufixed"):
         return float
 
     raise ValueError(f"cannot convert '{arg_type}' from string")
 
 
-def argument_parsers_for_params(
+def _argument_parsers_for_params(
     outputs: Sequence[ABIFunctionParams],
 ) -> List[ParamParser]:
     """
@@ -79,32 +126,70 @@ def argument_parsers_for_params(
 
     out_types: List[ParamParser] = []
     for output in outputs:
-        out_types.append(string_to_argument_fn_for_type(output["type"]))
+        out_types.append(_string_to_argument_fn_for_type(output["type"]))
 
     return out_types
 
 
-def parse_function_arguments(
-    abi: ABI, function_name: str, arguments: List[str]
-) -> List[Any]:
+def _parse_return_value_from_type(
+    type_name: str, output: ABIFunctionParams, value: Any
+) -> Any:
     """
-    Given an ABI, a function name and a list of string parameters,
-    parse the parameters to suitable types to call the function on a
-    contract with the given ABI.
+    Parse a single value from an ABIFunctionParams.
     """
 
-    for element in abi:
+    # Check for array types
+    if type_name.endswith("[]"):
+        assert isinstance(value, list)
+        element_type = type_name[:-2]
+        return [_parse_return_value_from_type(element_type, output, v) for v in value]
 
-        if element["type"] != "function" or element["name"] != function_name:
-            continue
+    # Check for tuples
+    if type_name == "tuple":
+        assert isinstance(value, tuple)
+        assert "components" in output
+        return _parse_return_value_tuple(output["components"], value)
 
-        inputs = element["inputs"]
-        parsers = argument_parsers_for_params(inputs)
-        if len(parsers) != len(arguments):
-            raise ValueError(
-                f"function requires {len(parsers)}, received {len(arguments)}"
-            )
+    return value
 
-        return [parse(arg) for parse, arg in zip(parsers, arguments)]
 
-    raise ValueError(f"function {function_name} not found in ABI")
+def _parse_return_value_as_anonymous_tuple(
+    outputs: Sequence[ABIFunctionParams], values: Tuple
+) -> Tuple:
+    """
+    Parse a list of unnamed ABIFunctionParams and a tuple, to a tuple.
+    """
+    assert len(values) == len(outputs)
+    return tuple(
+        _parse_return_value_from_type(out["type"], out, val)
+        for val, out in zip(values, outputs)
+    )
+
+
+def _parse_return_value_as_named_tuple(
+    outputs: Sequence[ABIFunctionParams], values: Tuple
+) -> Dict[str, Any]:
+    """
+    Parse a list of named ABIFunctionParams and a tuple, to a dict.
+    """
+
+    assert len(values) == len(outputs)
+    value_dict: Dict[str, Any] = {}
+    for val, out in zip(values, outputs):
+        value_dict[out["name"]] = _parse_return_value_from_type(out["type"], out, val)
+
+    return value_dict
+
+
+def _parse_return_value_tuple(
+    outputs: Sequence[ABIFunctionParams], values: Tuple
+) -> Any:
+    """
+    Anonymous tuples to tuples, named tuples to dictionaries.
+    """
+
+    assert len(values) == len(outputs)
+    if outputs[0]["name"] == "":
+        return _parse_return_value_as_anonymous_tuple(outputs, values)
+
+    return _parse_return_value_as_named_tuple(outputs, values)
--- tests/test_abi_parser.py
+++ tests/test_abi_parser.py
@@ -2,11 +2,105 @@
 Tests for abi_parser.py
 """
 
-from autonity.abi_parser import parse_function_arguments
+from autonity.abi_parser import find_abi_function, parse_arguments, parse_return_value
 from autonity.abi_manager import ABIManager
 
 from web3 import Web3
+from web3.types import ABI
 from unittest import TestCase
+from typing import Dict, cast
+
+
+MOCK_ABI = cast(
+    ABI,
+    [
+        {
+            "inputs": [{"internalType": "address", "name": "_addr", "type": "address"}],
+            "name": "getValidator",
+            "outputs": [
+                {
+                    "components": [
+                        {
+                            "internalType": "address payable",
+                            "name": "treasury",
+                            "type": "address",
+                        },
+                        {"internalType": "address", "name": "addr", "type": "address"},
+                        {"internalType": "string", "name": "enode", "type": "string"},
+                        {
+                            "internalType": "uint256",
+                            "name": "commissionRate",
+                            "type": "uint256",
+                        },
+                        {
+                            "internalType": "uint256",
+                            "name": "bondedStake",
+                            "type": "uint256",
+                        },
+                        {
+                            "internalType": "uint256",
+                            "name": "totalSlashed",
+                            "type": "uint256",
+                        },
+                        {
+                            "internalType": "contract Liquid",
+                            "name": "liquidContract",
+                            "type": "address",
+                        },
+                        {
+                            "internalType": "uint256",
+                            "name": "liquidSupply",
+                            "type": "uint256",
+                        },
+                        {
+                            "internalType": "uint256",
+                            "name": "registrationBlock",
+                            "type": "uint256",
+                        },
+                        {
+                            "internalType": "enum Autonity.ValidatorState",
+                            "name": "state",
+                            "type": "uint8",
+                        },
+                    ],
+                    "internalType": "struct Autonity.Validator",
+                    "name": "",
+                    "type": "tuple",
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function",
+        }
+    ],
+)
+
+MOCK_RETURN_VALUE = (
+    "0x75474aC55768fAb6fE092191eea8016b955072F5",
+    "0x32F3493Ef14c28419a98Ff20dE8A033cf9e6aB97",
+    "enode://d4dc137f987e17155a69b31e566494c16edafd228912483cc519a48ce85864781"
+    "faccc38141cc0eb1df8cdb28b9b3ccd10e1c298bac78ac43bbe5804021c1152@34.142.71.5:30303",
+    1000,
+    10000000000000000000000,
+    0,
+    "0xf4D9599aFd90B5038b18e3B551Bc21a97ed21c37",
+    10000000000000000000000,
+    0,
+    0,
+)
+
+MOCK_EXPECTED_PARSED_RETURN_VALUE: Dict = {
+    "treasury": "0x75474aC55768fAb6fE092191eea8016b955072F5",
+    "addr": "0x32F3493Ef14c28419a98Ff20dE8A033cf9e6aB97",
+    "enode": "enode://d4dc137f987e17155a69b31e566494c16edafd228912483cc519a48ce85864781"
+    "faccc38141cc0eb1df8cdb28b9b3ccd10e1c298bac78ac43bbe5804021c1152@34.142.71.5:30303",
+    "commissionRate": 1000,
+    "bondedStake": 10000000000000000000000,
+    "totalSlashed": 0,
+    "liquidContract": "0xf4D9599aFd90B5038b18e3B551Bc21a97ed21c37",
+    "liquidSupply": 10000000000000000000000,
+    "registrationBlock": 0,
+    "state": 0,
+}
 
 
 class TestABIParser(TestCase):
@@ -20,8 +114,9 @@ class TestABIParser(TestCase):
         """
 
         abi = ABIManager.load_abi("IERC20")
-        arguments = parse_function_arguments(
-            abi, "approve", ["0x2b5ad5c4795c026514f8317c7a215e218dccd6cf", "100"]
+        abi_function = find_abi_function(abi, "approve")
+        arguments = parse_arguments(
+            abi_function, ["0x2b5ad5c4795c026514f8317c7a215e218dccd6cf", "100"]
         )
         self.assertEqual(
             [Web3.toChecksumAddress("0x2b5ad5c4795c026514f8317c7a215e218dccd6cf"), 100],
@@ -29,3 +124,14 @@ class TestABIParser(TestCase):
         )
 
         # TODO: use mock ABIs to test other types
+
+    def test_return_value_decoding(self) -> None:
+        """
+        Test return value decoding.
+        """
+
+        abi_function = find_abi_function(MOCK_ABI, "getValidator")
+        retval = parse_return_value(abi_function, MOCK_RETURN_VALUE)
+        self.assertEqual(MOCK_EXPECTED_PARSED_RETURN_VALUE, retval)
+
+        # TODO: test more sophisticated cases
