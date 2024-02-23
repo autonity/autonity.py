2ccb52d075fae7b5963227487ecc78c06c8229a2
Aiman Baharna <ab@clearmatics.com>
2024-02-23 13:36:15 +0000

    Revise update-abi.sh script and documentation
    Optionally take a Git commit ID or Git tag as an argument, otherwise
    fall back to `autonity-commit.txt`. Fix an issue where the
    `autonity-commit.txt` was being read from the wrong path.
    
    Also, update the outdated section in the README.md to reflect current
    state.

--- scripts/update-abi.sh
+++ scripts/update-abi.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/bash -eu
 
 REPO_DIR="$(realpath "$(dirname "$0")/..")"
+ABI_DIR="${REPO_DIR}/autonity/abi"
 WORKING_DIR="$(mktemp -d)"
 
-AUTONITY_DIR="${WORKING_DIR}/autonity"
-ABI_DIR="${AUTONITY_DIR}/abi"
-
-AUTONITY_COMMIT="$(cat ${ABI_DIR}/autonity-commit.txt)"
+AUTONITY_COMMIT="${1:-$(cat ${ABI_DIR}/autonity-commit.txt)}"
 AUTONITY_REPO_URL='https://github.com/autonity/autonity.git'
 
-git clone ${AUTONITY_REPO_URL} "${AUTONITY_DIR}"
-cd "${AUTONITY_DIR}"
+if [[ $# -gt 1 ]]; then
+    echo "usage: $0 [AUTONITY_COMMIT]"
+    exit 1
+fi
+
+git clone ${AUTONITY_REPO_URL} "${WORKING_DIR}"
+cd "${WORKING_DIR}"
 git checkout ${AUTONITY_COMMIT}
 make contracts
 for abi_file in ./params/generated/*.abi; do
 	jq --sort-keys <${abi_file} >"${ABI_DIR}/${abi_file##*/}"
 done
-git -C ${AUTONITY_DIR} log --no-decorate --pretty='format:%H' -n 1 >${ABI_DIR}/autonity-commit.txt
+git -C ${WORKING_DIR} log --no-decorate --pretty='format:%H' -n 1 >${ABI_DIR}/autonity-commit.txt
 rm -rf ${WORKING_DIR}
