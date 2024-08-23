ef3c25f40730806aa152dd14df5b97cfc7bdfad0
Máté Szendrő <mate.szendro@clearmatics.com>
2024-08-23 13:23:44 +0100

    Refactor: Store Autonity constants in one place in the repository
    Instead of specifying the Autonity contract address in the Makefile and
    copying it into the generated binding, specify the contract address in
    the factory function where other bindings also get their contract
    address.
    
    Also move the native token symbol from networks module as it's the same
    in all networks.

---
+++ autonity/constants.py
@@ -0,0 +1,11 @@
+from typing import cast
+
+from eth_typing import ChecksumAddress
+
+AUTONITY_CONTRACT_ADDRESS = cast(
+    ChecksumAddress, "0xBd770416a3345F91E4B34576cb804a576fa48EB1"
+)
+AUTONITY_CONTRACT_VERSION = 1
+
+NATIVE_TOKEN_SYMBOL = "ATN"
+NATIVE_TOKEN_DECIMALS = 18
