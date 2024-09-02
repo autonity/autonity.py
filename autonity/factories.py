e64e5047fb31c79f3f28587aaecc7e00b6b7bed6
Máté Szendrő <mate.szendro@clearmatics.com>
2024-09-02 16:11:08 +0200

    Add docstrings to the rest of the API modules

--- autonity/factories.py
+++ autonity/factories.py
@@ -1,3 +1,7 @@
+"""Factory functions that return instances of Autonity protocol contract wrappers bound
+to the current contract addresses.
+"""
+
 from functools import lru_cache
 from warnings import warn
 
@@ -41,52 +45,174 @@ def _config(w3: Web3) -> autonity.Config:
 
 
 def Accountability(w3: Web3) -> accountability.Accountability:
+    """Accountability contract wrapper factory.
+
+    Parameters
+    ----------
+    w3 : web3.Web3
+        A `web3.Web3` instance with a provider connected to an Autonity network.
+
+    Returns
+    -------
+    autonity.contracts.accountability.Accountability
+        A contract wrapper instance.
+    """
     return accountability.Accountability(
         w3, _config(w3).contracts.accountability_contract
     )
 
 
 def ACU(w3: Web3) -> acu.ACU:
+    """ACU contract wrapper factory.
+
+    Parameters
+    ----------
+    w3 : web3.Web3
+        A `web3.Web3` instance with a provider connected to an Autonity network.
+
+    Returns
+    -------
+    autonity.contracts.acu.ACU
+        A contract wrapper instance.
+    """
     return acu.ACU(w3, _config(w3).contracts.acu_contract)
 
 
 def Autonity(w3: Web3) -> autonity.Autonity:
+    """Autonity contract wrapper factory.
+
+    Parameters
+    ----------
+    w3 : web3.Web3
+        A `web3.Web3` instance with a provider connected to an Autonity network.
+
+    Returns
+    -------
+    autonity.contracts.autonity.Autonity
+        A contract wrapper instance.
+    """
     assert _config(w3)
     return autonity.Autonity(w3, AUTONITY_CONTRACT_ADDRESS)
 
 
 def InflationController(w3: Web3) -> inflation_controller.InflationController:
+    """InflationController contract wrapper factory.
+
+    Parameters
+    ----------
+    w3 : web3.Web3
+        A `web3.Web3` instance with a provider connected to an Autonity network.
+
+    Returns
+    -------
+    autonity.contracts.inflation_controller.InflationController
+        A contract wrapper instance.
+    """
     return inflation_controller.InflationController(
         w3, _config(w3).contracts.inflation_controller_contract
     )
 
 
 def Liquid(w3: Web3, address: ChecksumAddress) -> liquid.Liquid:
+    """Liquid contract wrapper factory.
+
+    Parameters
+    ----------
+    w3 : web3.Web3
+        A `web3.Web3` instance with a provider connected to an Autonity network.
+    address : eth_typing.ChecksumAddress
+        The address of a deployed Liquid contract.
+
+    Returns
+    -------
+    autonity.contracts.liquid.Liquid
+        A contract wrapper instance.
+    """
     assert _config(w3)
     return liquid.Liquid(w3, address)
 
 
 def NonStakableVesting(w3: Web3) -> non_stakable_vesting.NonStakableVesting:
+    """NonStakableVesting contract wrapper factory.
+
+    Parameters
+    ----------
+    w3 : web3.Web3
+        A `web3.Web3` instance with a provider connected to an Autonity network.
+
+    Returns
+    -------
+    autonity.contracts.non_stakable_vesting.NonStakableVesting
+        A contract wrapper instance.
+    """
     return non_stakable_vesting.NonStakableVesting(
         w3, _config(w3).contracts.non_stakable_vesting_contract
     )
 
 
 def Oracle(w3: Web3) -> oracle.Oracle:
+    """Oracle contract wrapper factory.
+
+    Parameters
+    ----------
+    w3 : web3.Web3
+        A `web3.Web3` instance with a provider connected to an Autonity network.
+
+    Returns
+    -------
+    autonity.contracts.oracle.Oracle
+        A contract wrapper instance.
+    """
     return oracle.Oracle(w3, _config(w3).contracts.oracle_contract)
 
 
 def Stabilization(w3: Web3) -> stabilization.Stabilization:
+    """Stabilization contract wrapper factory.
+
+    Parameters
+    ----------
+    w3 : web3.Web3
+        A `web3.Web3` instance with a provider connected to an Autonity network.
+
+    Returns
+    -------
+    autonity.contracts.stabilization.Stabilization
+        A contract wrapper instance.
+    """
     return stabilization.Stabilization(w3, _config(w3).contracts.stabilization_contract)
 
 
 def SupplyControl(w3: Web3) -> supply_control.SupplyControl:
+    """SupplyControl contract wrapper factory.
+
+    Parameters
+    ----------
+    w3 : web3.Web3
+        A `web3.Web3` instance with a provider connected to an Autonity network.
+
+    Returns
+    -------
+    autonity.contracts.supply_control.SupplyControl
+        A contract wrapper instance.
+    """
     return supply_control.SupplyControl(
         w3, _config(w3).contracts.supply_control_contract
     )
 
 
 def UpgradeManager(w3: Web3) -> upgrade_manager.UpgradeManager:
+    """UpgradeManager contract wrapper factory.
+
+    Parameters
+    ----------
+    w3 : web3.Web3
+        A `web3.Web3` instance with a provider connected to an Autonity network.
+
+    Returns
+    -------
+    autonity.contracts.upgrade_manager.UpgradeManager
+        A contract wrapper instance.
+    """
     return upgrade_manager.UpgradeManager(
         w3, _config(w3).contracts.upgrade_manager_contract
     )
