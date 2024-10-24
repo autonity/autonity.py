ebee76e013182c68b4448fd5fa52c6779d8643d4
Máté Szendrő <mate.szendro@clearmatics.com>
2024-10-24 13:08:28 +0200

    Don't enter Hatch shell at devenv enterShell
    Port https://github.com/autonity/autonity-cli/pull/174/commits/5df2d1f0b1e7cef7a502d32abd01a59657cb2183
    to autonity.py.

--- devenv.nix
+++ devenv.nix
@@ -10,11 +10,4 @@
     version = "3.12";
     venv.enable = false;
   };
-
-  enterShell = ''
-    hatch run true  # Create virtualenv at first run
-    export VIRTUAL_ENV="$(hatch env find)"
-    export HATCH_ENV_ACTIVE=default
-    export PATH="$VIRTUAL_ENV/bin:$PATH"
-  '';
 }
