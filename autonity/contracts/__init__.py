9a3d688334952ba77e02b06ab166f894b7d73d36
Máté Szendrő <mate.szendro@clearmatics.com>
2026-02-18 11:06:27 +0100

    Remove unused plum dependency
    There aren't any overloaded contract functions in the current version.

--- autonity/contracts/__init__.py
+++ autonity/contracts/__init__.py
@@ -3,10 +3,3 @@
 Each module consists of the contract binding class, the contract ABI, and ports of
 Solidity structures and enumerations.
 """
-
-import warnings
-
-# Suppress warnings raised by `plum` about not being able to resolve types
-# from `eth_typing`; the dispatch works as expected
-warnings.filterwarnings("ignore", message="Could not resolve the type hint")
-warnings.filterwarnings("ignore", message="Could not determine whether .+ is faithful")
